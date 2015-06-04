09e4aa52444fbeac7d44e3d312ed51f2d8b25a5a
Benjamin <benjamin@Debian-jessie-amd64-netboot.vagrantup.com>
2015-06-05 04:21:54 +1000

    Update

---
+++ src/Forge/Http.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Forge;
+
+/*
+ * Http Protocol
+ *
+ * Http Protocol, at this stage just hosting response codes for Application
+ *
+ * @see http://www.w3.org/Protocols/rfc2616/rfc2616-sec6.html#sec6
+ * 
+ * @author     Benjamin Charles Tehan <benjamin.tehan@devforge.net>
+ * @version    $Id:$
+ * @copyright  Copyright (c) 2013 devforge.net All rights reserved.
+ */
+class Http {
+
+	/*
+	 * @const int
+	 */
+	const STATUS_CODE_100 = 100;
+	const STATUS_CODE_101 = 101;
+	const STATUS_CODE_200 = 200;
+	const STATUS_CODE_201 = 201;
+	const STATUS_CODE_202 = 202;
+	const STATUS_CODE_203 = 203;
+	const STATUS_CODE_204 = 204;
+	const STATUS_CODE_205 = 205;
+	const STATUS_CODE_206 = 206;
+	const STATUS_CODE_300 = 300;
+	const STATUS_CODE_301 = 301;
+	const STATUS_CODE_302 = 302;
+	const STATUS_CODE_303 = 303;
+	const STATUS_CODE_304 = 304;
+	const STATUS_CODE_305 = 305;
+	const STATUS_CODE_307 = 307;
+	const STATUS_CODE_400 = 400;
+	const STATUS_CODE_401 = 401;
+	const STATUS_CODE_402 = 402;
+	const STATUS_CODE_403 = 403;
+	const STATUS_CODE_404 = 404;
+	const STATUS_CODE_405 = 405;
+	const STATUS_CODE_406 = 406;
+	const STATUS_CODE_407 = 407;
+	const STATUS_CODE_408 = 408;
+	const STATUS_CODE_409 = 409;
+	const STATUS_CODE_410 = 410;
+	const STATUS_CODE_411 = 411;
+	const STATUS_CODE_412 = 412;
+	const STATUS_CODE_413 = 413;
+	const STATUS_CODE_414 = 414;
+	const STATUS_CODE_415 = 415;
+	const STATUS_CODE_416 = 416;
+	const STATUS_CODE_417 = 417;
+	const STATUS_CODE_500 = 500;
+	const STATUS_CODE_501 = 501;
+	const STATUS_CODE_502 = 502;
+	const STATUS_CODE_503 = 503;
+	const STATUS_CODE_504 = 504;
+	const STATUS_CODE_505 = 505;
+}
